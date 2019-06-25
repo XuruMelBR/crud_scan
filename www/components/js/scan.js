7f4213b515c3547bd3fdb50f67110c0826c945d5
XuruMelBR <santaniellicasa@gmail.com>
2019-06-26 03:36:44 +0900

    grrrrcomitando o js novamente

--- www/components/js/scan.js
+++ www/components/js/scan.js
@@ -1,2 +1,20 @@
 // This is a JavaScript file
+//comitando denovo sicarai
+function scanBarCode() 
+{
+    window.plugins.barcodeScanner.scan(function(result) 
+    {
+        document.getElementById("titulo-codigo").textContent=result.text;
+    },function(error) 
+      {
+          alert("Erro ao escanear: " + error);
+      });
+}
 
+function scanUpdate()
+{
+  var resultado;
+  resultado = document.getElementById("codigo").textContent;
+
+
+}
